08c5cf78630e904133951e8fe4ef4f1a4dba9aff
Hlormar <barneycalhoon98@gmail.com>
2025-02-02 03:07:38 +0500

    Accessibility & security patch

--- parser.py
+++ parser.py
@@ -1,17 +1,28 @@
 import requests
 import sys
+import json
 from datetime import datetime, timedelta
 
 
+
+LOGIN_URL = "https://lms.utmn.ru/login/token.php"
+CALENDAR_URL = "https://lms.utmn.ru/webservice/rest/server.php"
+
 s = requests.session()
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) \
+        Chrome/91.0.4472.124 Safari/537.36"}
 
 
-def getToken(user, password):
+def getToken(user, password):  # returns token string
     try:
         r = s.post(
-            "https://lms.utmn.ru/login/token.php",
-            json={"username": user, "password": password, "service": "test"})
-        return r.text.split("token")[1][3:-10]
+            url = LOGIN_URL,
+            json = {"username": user, "password": password, "service": "test"},
+            headers = headers)
+
+        token_data = r.json()
+        return token_data.get("token", None)
 
     # connection problems
     except requests.ConnectionError:
@@ -23,11 +34,18 @@ def getToken(user, password):
         return (-1)
 
 
-def getCalendar(token):
+def getCalendar(token):  # returns raw calendar response
     try:
-        r = s.post(url="https://lms.utmn.ru/webservice/rest/server.php?\
-                wsfunction=core_calendar_get_calendar_upcoming_view&wstoken="
-                   + token + "&moodlewsrestformat=json")
+        params = {
+            'wsfunction': 'core_calendar_get_calendar_upcoming_view',
+            'wstoken': token,
+            'moodlewsrestformat': 'json'}
+
+        r = s.post(
+            url = CALENDAR_URL,
+            params = params,
+            headers = headers)
+
         return r.text
 
     # connection problems
@@ -44,61 +62,53 @@ def getCalendar(token):
         return (-1)
 
 
-def formatDict(raw):
+def formatDict(raw):  #returns json dictioanary
     try:
-        """# cut the long base64 image part
-        courseIndex = raw.find('"courseimage"')
-        progressIndex = raw.find('"progress"')
-        while courseIndex != -1:
-            raw = raw[:courseIndex] + raw[progressIndex:]
-            progressIndex = raw.find('"progress"', courseIndex+1)
-            courseIndex = raw.find('"courseimage"')"""
-
-        # replace \/ with /
-        slashIndex = raw.find('\\/')
-        while slashIndex != -1:
-            raw = raw[:slashIndex] + raw[slashIndex+1:]
-            slashIndex = raw.find('\\/', slashIndex+1)
-
-        null = "None"
-        false = "False"
-        true = "True"
-        return (eval(raw))  # returns a dictionary
+        raw = raw.replace("\\/", "/")
+        null = "null"
+        false = "false"
+        true = "true"
+        return json.loads(raw)
 
     # no data
     except AttributeError:
         print("formatDict: No data")
         return (-1)
+    # invalid JSON
+    except json.JSONDecodeError:
+        print("formatDict: Invalid JSON format")
+        return(-1)
     # unknown
     except Exception as e:
         print("formatDict:", e)
         return (-1)
 
 
-def parseValues(calendar):
+def buildOutput(calendar):
     # recieves a python dictionary (output of the formatDict())
     try:
-        events = calendar['events']  # an array of a dictionaries
+        events = calendar.get('events', [])  # list of a dictionaries
         out = ""
         for event in events:
             start = convertTime(event['timestart'])
             duration = str(timedelta(seconds=event['timeduration']))
             modified = convertTime(event['timemodified'])
 
-
-            out += ('COURSE:\t\t\t' + event['course']['fullnamedisplay'] +
-                  '\nTASK:\t\t\t' + event['name'] +
-                  '\nDESCRIPTION:\t\t' + event['description'] +
-                  '\nEVENT TYPE:\t\t' + event['eventtype'] +
-                  '\nSTART TIME:\t\t' + start +
-                  '\nDURATION:\t\t' + duration +
-                  '\nMODIFICATION DATE:\t' + modified +
-                  '\nCALENDAR URL:\t\t' + event['viewurl'] +
-                  '\nCOURSE URL:\t\t' + event['course']['viewurl'] +
-                  '\nHAS PROGRESS?:\t\t' + event['course']['hasprogress'] +
-                  '\nPROGRESS:\t\t' + str(event['course']['progress']) + "\n\n")
+            out += (
+                f'COURSE:\t\t\t{event["course"]["fullnamedisplay"]}\n'
+                f'TASK:\t\t\t{event["name"]}\n'
+                f'DESCRIPTION:\t\t{event["description"]}\n'
+                f'EVENT TYPE:\t\t{event["eventtype"]}\n'
+                f'START TIME:\t\t{start}\n'
+                f'DURATION:\t\t{duration}\n'
+                f'MODIFICATION DATE:\t{modified}\n'
+                f'CALENDAR URL:\t\t{event["viewurl"]}\n'
+                f'COURSE URL:\t\t{event["course"]["viewurl"]}\n'
+                f'HAS PROGRESS?:\t\t{event["course"]["hasprogress"]}\n'
+                f'PROGRESS:\t\t{event["course"]["progress"]}\n\n')
         return out
-
+    except KeyError as ke:
+        print("parseValue: Missing key -", ke)
     except TypeError:
         print("parseValue: No data or certain value")
         return (-1)
@@ -117,8 +127,19 @@ def convertTime(timestampStr):
 if __name__ == '__main__':
     try:
         token = getToken(sys.argv[1], sys.argv[2])
+        if token == -1:
+            print("main: Failed to retrieve token")
+            exit(-1)
+        calendarRaw = getCalendar(token)
+        if calendarRaw == -1:
+            print("main: Failed to retrieve calendar")
+            exit(-1)
+        calendarDict = formatDict(getCalendar(token))
+        if calendarDict == -1:
+            print("main: Failed to format calendar data")
+            exit(-1)
+        print(buildOutput(calendarDict))
+
     except IndexError:
         print("main: No credentials")
         exit()
-    calendar = formatDict(getCalendar(token))
-    print(parseValues(calendar))
